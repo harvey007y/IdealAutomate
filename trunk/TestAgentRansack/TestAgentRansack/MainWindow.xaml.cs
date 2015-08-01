148858df51547e2516941ce231b525ff60449b64
harvey007 <harvey007@124d8473-603e-4c5d-b26b-045a8a2a3c4f>
2015-08-01 13:54:43 +0000

    Using timer to get global hot key for pause

--- trunk/TestAgentRansack/TestAgentRansack/MainWindow.xaml.cs
+++ trunk/TestAgentRansack/TestAgentRansack/MainWindow.xaml.cs
@@ -18,12 +18,7 @@ namespace TestAgentRansack {
 };
       window.Show();
       IdealAutomate.Core.Methods myActions = new Methods();
-      // Register Pause Key as globalhotkey to allow user to stop
-      // script from executing. Pause key will kill all processes with
-      // assembly description of IdealAutomateScript. It does this
-      // allow you to stop any child scripts that were launched from
-      // a parent script
-      myActions.RegisterHotKey(window); 
+
       InitializeComponent();
       this.Hide();
       myActions.Run(@"c:\Program Files\Mythicsoft\Agent Ransack\AgentRansack.exe","");
