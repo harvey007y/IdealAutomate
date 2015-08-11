d9a0109cf3f72a63fc838be137d828edd0ea9d98
harvey007 <harvey007@124d8473-603e-4c5d-b26b-045a8a2a3c4f>
2015-08-11 15:54:52 +0000

    Initial Build

---
+++ ClearCache/ClearCache/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace ClearCache {
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : Application {
+    protected override void OnStartup(StartupEventArgs e) {
+      base.OnStartup(e);
+      DispatcherUnhandledException += app_DispatcherUnhandledException;
+    }
+    static void app_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e) {
+      MessageBox.Show(e.Exception.InnerException.ToString());
+      e.Handled = true;
+      // Log/inspect the inspection here
+    }
+  }
+}
