b332c72d055a1f3359254a876014a38dddaa4e64
harvey007y <harvey007y@gmail.com>
2017-06-03 09:24:34 -0500

    test commit to test branch

--- CodeGenTemplateParms/CodeGenTemplateParms/MainWindow.xaml.cs
+++ CodeGenTemplateParms/CodeGenTemplateParms/MainWindow.xaml.cs
@@ -11,7 +11,7 @@ using System.Reflection;
 
 namespace CodeGenTemplateParms {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MainWindow.xaml.
     /// </summary>
     public partial class MainWindow : Window {
         public MainWindow() {
