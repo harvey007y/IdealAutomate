5b49b460fec10457e7ac20b1d97c1d2aa3e21029
harvey007y <harvey007y@gmail.com>
2017-08-05 18:00:40 -0500

    Initial Commit

---
+++ IdealAutomateExplorer/IdealAutomateExplorer/StringExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.Windows.Forms.Samples
+{
+    public static class StringExtensions
+    {
+        public static string Repeat(this char chatToRepeat, int repeat)
+        {
+
+            return new string(chatToRepeat, repeat);
+        }
+        public static string Repeat(this string stringToRepeat, int repeat)
+        {
+            var builder = new StringBuilder(repeat * stringToRepeat.Length);
+            for (int i = 0; i < repeat; i++)
+            {
+                builder.Append(stringToRepeat);
+            }
+            return builder.ToString();
+        }
+    }
+}
