124609fafbfa9e576b486200d5d2807f3ed2e456
harvey007y <harvey007y@gmail.com>
2017-09-18 16:32:03 -0500

    Added VisualStudioHotKeys

---
+++ VisualStudio/VisualStudioHotKeys/VisualStudioHotKeys/App.xaml.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace VisualStudioHotKeys {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+    }
+}
