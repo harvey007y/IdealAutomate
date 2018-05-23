5cd71c61347fdb632d1d328daa523adeb93bc9e8
harvey007y <harvey007y@gmail.com>
2018-05-23 18:13:05 -0500

    Wrapped IdealSqlTracer registry request for servers in try catch to help debug problem

--- IdealAutomateExplorer/IdealSqlTracer/MainWindow.xaml.cs
+++ IdealAutomateExplorer/IdealSqlTracer/MainWindow.xaml.cs
@@ -145,39 +145,46 @@ namespace IdealSqlTracer {
                 List<string> listLocalServers = new List<string>();
 
                 // Get servers from the registry (if any)
-                RegistryKey key = RegistryKey.OpenBaseKey(
-                  Microsoft.Win32.RegistryHive.LocalMachine, RegistryView.Registry32);
-                key = key.OpenSubKey(@"SOFTWARE\Microsoft\Microsoft SQL Server");
-                object installedInstances = null;
-                if (key != null) { installedInstances = key.GetValue("InstalledInstances"); }
-                List<string> instances = null;
-                if (installedInstances != null) { instances = ((string[])installedInstances).ToList(); }
-                if (System.Environment.Is64BitOperatingSystem) {
-                    /* The above registry check gets routed to the syswow portion of 
-                     * the registry because we're running in a 32-bit app. Need 
-                     * to get the 64-bit registry value(s) */
-                    key = RegistryKey.OpenBaseKey(
-                            Microsoft.Win32.RegistryHive.LocalMachine, RegistryView.Registry64);
+                try {
+                    RegistryKey key = RegistryKey.OpenBaseKey(
+                              Microsoft.Win32.RegistryHive.LocalMachine, RegistryView.Registry32);
                     key = key.OpenSubKey(@"SOFTWARE\Microsoft\Microsoft SQL Server");
-                    installedInstances = null;
+                    object installedInstances = null;
                     if (key != null) { installedInstances = key.GetValue("InstalledInstances"); }
-                    string[] moreInstances = null;
-                    if (installedInstances != null) {
-                        moreInstances = (string[])installedInstances;
-                        if (instances == null) {
-                            instances = moreInstances.ToList();
-                        } else {
-                            instances.AddRange(moreInstances);
+                    List<string> instances = null;
+                    if (installedInstances != null) { instances = ((string[])installedInstances).ToList(); }
+                    if (System.Environment.Is64BitOperatingSystem) {
+                        /* The above registry check gets routed to the syswow portion of 
+                         * the registry because we're running in a 32-bit app. Need 
+                         * to get the 64-bit registry value(s) */
+                        key = RegistryKey.OpenBaseKey(
+                                Microsoft.Win32.RegistryHive.LocalMachine, RegistryView.Registry64);
+                        key = key.OpenSubKey(@"SOFTWARE\Microsoft\Microsoft SQL Server");
+                        installedInstances = null;
+                        if (key != null) { installedInstances = key.GetValue("InstalledInstances"); }
+                        string[] moreInstances = null;
+                        if (installedInstances != null) {
+                            moreInstances = (string[])installedInstances;
+                            if (instances == null) {
+                                instances = moreInstances.ToList();
+                            } else {
+                                instances.AddRange(moreInstances);
+                            }
                         }
                     }
-                }
-                foreach (string item in instances) {
-                    string name = System.Environment.MachineName;
-                    if (item != "MSSQLSERVER") { name += @"\" + item; }
-                    if (!servers.Contains(name.ToUpper())) {
-                        myServers.Add(name.ToUpper());
-                        listLocalServers.Add(name.ToUpper());
+                    foreach (string item in instances) {
+                        string name = System.Environment.MachineName;
+                        if (item != "MSSQLSERVER") { name += @"\" + item; }
+                        if (!servers.Contains(name.ToUpper())) {
+                            myServers.Add(name.ToUpper());
+                            listLocalServers.Add(name.ToUpper());
+                        }
                     }
+                } catch (Exception ex) {
+
+                    MessageBox.Show(ex.Message);
+                    MessageBox.Show(ex.StackTrace);
+                    MessageBox.Show(ex.InnerException.ToString());
                 }
 
                 try {
