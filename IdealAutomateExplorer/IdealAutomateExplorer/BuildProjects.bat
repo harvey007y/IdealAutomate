474e2f319297307fb737d1173ef0e1e867ad03b4
Wade Harvey <harvey007y@gmail.com>
2021-12-26 09:58:47 -0600

    Added vswhere to build.bat to find msbuild on computer

--- IdealAutomateExplorer/IdealAutomateExplorer/BuildProjects.bat
+++ IdealAutomateExplorer/IdealAutomateExplorer/BuildProjects.bat
@@ -2,10 +2,12 @@
 rem I had to delete environmental variable Platform that had value of BPC and reboot to get this to work because it was overridding something
 SET myFILE=%1
 
-
+SET myBuild="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -prerelease -products * -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe
 
   "nuget.exe restore %myFILE%"
-  "C:\Program Files (x86)\MSBuild\14.0\Bin\MSbuild.exe" /p:Configuration=Debug %myFILE%
+  echo %myFile%
+  for /f "tokens=*" %%i in ('%myBuild%') do set RESULT=%%i
+ "%RESULT%" /p:Configuration=Debug %myFILE%
   echo "!!!!!! %myFILE% has completed !!!!!!"
   pause
   
